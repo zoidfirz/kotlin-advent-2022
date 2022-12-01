f23ae0a084123f56d5235749a6f90e126177853f
Jason Sutherland <jason.k.sutherland@gmail.com>
2022-12-01 02:11:20 -0500

    day 01 part 2 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -8,10 +8,26 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput("Day01Input")
+    var highNumber: Int = 0
+    var arrOfCalories:MutableList<Int> = ArrayList()
+    var sum:Int = 0
+    for (i in input.indices){
+        if (input[i] != "") {
+            sum += input[i].toInt()
+        } else {
+            arrOfCalories.add(sum)
+            sum = 0
+        }
+    }
+
+    arrOfCalories.sort()
+    arrOfCalories.reverse()
+    println(arrOfCalories[0]+arrOfCalories[1]+ arrOfCalories[2])
+
+//    println(part1(input))
+//    println(part2(input))
 }
